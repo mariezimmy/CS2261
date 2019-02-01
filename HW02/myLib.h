4c4d8d6f879933ed7ec5bd225a5c24de4c00a7a0
mariezimmy <mariezimmy@users.noreply.github.com>
2019-02-01 13:26:47 -0500

    nit: fixed .h file to reflect most recent methods

--- HW02/myLib.h
+++ HW02/myLib.h
@@ -1,30 +1,30 @@
 #ifndef MY_LIB_H
 #define MY_LIB_H
 
-// Common Typedefs
+// common typedefs
 typedef unsigned short u16;
 
-// Common Macros
+// common macros
 #define OFFSET(r,c,rowlen) ((r)*(rowlen)+(c))
 
 // ================================= DISPLAY ==================================
 
-// Display Control Register
-#define REG_DISPCTL (*(unsigned short *)0x4000000)
+// display control register
+#define REG_DISPCTL (*(u16 *)0x4000000)
 #define MODE3 3
 #define BG2_ENABLE (1<<10)
 
-// Display Status Registers
-#define SCANLINECOUNTER (*(volatile unsigned short *)0x4000006)
+// display status registers
+#define SCANLINECOUNTER (*(volatile u16 *)0x4000006)
 
-// Display Constants
+// display Constants
 #define SCREENHEIGHT 160
 #define SCREENWIDTH 240
 
-// Video Buffer
-extern unsigned short *videoBuffer;
+// video buffer
+extern u16 *videoBuffer;
 
-// Color
+// color
 #define COLOR(r,g,b) ((r) | (g)<<5 | (b)<<10)
 #define BLACK 0
 #define WHITE COLOR(31,31,31)
@@ -35,21 +35,21 @@ extern unsigned short *videoBuffer;
 #define MAGENTA COLOR(31,0,31)
 #define YELLOW COLOR(31,31,0)
 
-// Mode 3 Drawing Functions
-void setPixel(int row, int col, unsigned short color);
-void drawRect(int row, int col, int height, int width, unsigned short color);
-void fillScreen(unsigned short color);
+// mode 3 drawing functions
+void setPixel(int row, int col, u16 color);
+void drawRect(int row, int col, int height, int width, u16 color);
+void drawSquare(int row, int col, int size, u16 color);
 
-// Miscellaneous Drawing Functions
+// miscellaneous drawing functions
 void waitForVBlank();
 
 
 // ================================== INPUT ===================================
 
-// Button Register
-#define BUTTONS (*(volatile unsigned short *)0x04000130)
+// button Register
+#define BUTTONS (*(volatile u16 *)0x04000130)
 
-// Button Masks
+// button Masks
 #define BUTTON_A		(1<<0)
 #define BUTTON_B		(1<<1)
 #define BUTTON_SELECT	(1<<2)
@@ -61,20 +61,22 @@ void waitForVBlank();
 #define BUTTON_R		(1<<8)
 #define BUTTON_L		(1<<9)
 
-// Variables for Button Macros
-extern unsigned short oldButtons;
-extern unsigned short buttons;
+// variables for button macros
+extern u16 oldButtons;
+extern u16 buttons;
 
-// Button Macros
-// TODO #3.0: Complete these macros
+// vutton Macros
 #define BUTTON_HELD(key) (~(BUTTONS) & (key))
 #define BUTTON_PRESSED(key) ((!(~(oldButtons) & (key))) && (~(buttons) & (key)))
 
 
 // ============================== MISCELLANEOUS ===============================
 
-// Miscellaneous Functions
-int collision(int rowA, int colA, int heightA, int widthA, int rowB, int colB, int heightB, int widthB);
-
+// miscellaneous functions
+int collision(int, int, int, int, int, int, int, int);
+void updateBallPosition(int);
+void updateBottomPaddlePosition();
+void updateTopPaddlePosition();
+void reset();
 
 #endif
